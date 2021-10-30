d5b8f949b44df11e0dcd14b53247baaea2156ab7
tanbiranjum <tanbiranjum@hotmail.com>
2021-10-31 03:01:30 +0600

    dark mode added

---
+++ main.js
@@ -0,0 +1,23 @@
+const darkModeBtn = document.querySelector('.dark-mode__btn')
+const body = document.querySelector('body')
+const container = document.querySelector('.container')
+
+darkModeBtn.addEventListener('click', () => {
+  darkModeBtn.innerHTML === 'OFF' ? btnToggle(true) : btnToggle(false)
+})
+
+const btnToggle = (isClicked) => {
+  if (isClicked) {
+    darkModeBtn.innerHTML = 'ON'
+    body.style.color = '#fff'
+    body.style.backgroundColor = 'black'
+    container.style.color = '#fff'
+    container.style.backgroundColor = 'black'
+    return
+  }
+  darkModeBtn.innerHTML = 'OFF'
+  body.style.color = '#444'
+  body.style.backgroundColor = 'rgb(219, 219, 219)'
+  container.style.color = '#444'
+  container.style.backgroundColor = '#fff'
+}
